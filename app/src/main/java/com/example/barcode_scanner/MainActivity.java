48f733c0db57f0ccef38159a96cb26c18355b6bf
JackCHG <gongjackiegong@gmail.com>
2021-03-27 14:02:35 -0700

    Started the implementation of the camerax api in the app

--- app/src/main/java/com/example/barcode_scanner/MainActivity.java
+++ app/src/main/java/com/example/barcode_scanner/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.barcode_scanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageCapture;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+  private ImageCapture imageCapture = null;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
