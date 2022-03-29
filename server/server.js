15217596940e600479cfe82e690af4b30ad5fa3d
JackCHG <gongjackiegong@gmail.com>
2022-03-29 09:29:34 -0700

    server.js runs. Added multer, async, express modules

--- server/server.js
+++ server/server.js
@@ -1,9 +1,38 @@
 "use strict";
 
-const port = 6400
+/* jshint node: true */
 
-const express = require('expresss')
-const app = express()
+const express = require('express');
+const app = express();
+//const session = require('express-session');
+//app.use(session({secret: 'secretKey', resave: false, saveUninitialized: false}));
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const async = require('async');
+
+const multer = require('multer');
+//const processFormBody = multer({storage: multer.memoryStorage()}).single('uploadedphoto');
+
+app.use(express.static(__dirname));
+
+/*
+ * This server exports the following URLs:
+ * /                    - Returns a test status message. Good for testing web
+ *                        server is running.
+ * 
+ *
+ */
+
+app.get('/', (request, response) => {
+  response.send('Simple web server of files from ' + __dirname);
+});
+
+const server = app.listen(3000, function() {
+  const port = server.address().port;
+  console.log('Listening at http://localhost:'
+              + port
+              + ' exporting the directory '
+              + __dirname);
+});
